1c4238a9717410da39b54496ce39976994457e29
Dennis Passway <dennispassway@gmail.com>
2020-07-20 15:40:22 +0200

    Move styles to seperate file

---
+++ src/components/miguelExampleStyles.js
@@ -0,0 +1,83 @@
+export const miguelExampleStyles = {
+  title: {
+    color: "#222",
+    fontFamily:
+      '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
+    fontSize: 30,
+    fontWeight: 700,
+    lineHeight: 1.2,
+    margin: "0 0 15px",
+  },
+  description: {
+    color: "#666",
+    fontFamily:
+      '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
+    fontSize: 18,
+    fontWeight: 400,
+    lineHeight: 1.7,
+    margin: "0 0 9px",
+  },
+  childrenContainerContainer: {
+    margin: "24px 0 0",
+  },
+  background: {
+    backgroundColor: "#fafafa",
+  },
+  container: {
+    backgroundColor: "#fff",
+    border: "solid 1px #ddd",
+    borderTopLeftRadius: 2,
+    borderTopRightRadius: 2,
+    overflow: "hidden",
+    padding: "20px 60px 20px 20px",
+    position: "relative",
+  },
+  children: {
+    overflow: "hidden",
+  },
+  dragButton: {
+    alignItems: "center",
+    borderLeft: "solid 1px #ddd",
+    color: "#aaa",
+    cursor: "ew-resize",
+    display: "flex",
+    height: "100%",
+    justifyContent: "center",
+    position: "absolute",
+    right: 0,
+    top: 0,
+    width: 40,
+  },
+  dragIcon: {
+    fill: "currentColor",
+    height: 20,
+    width: 20,
+  },
+  linkContainer: {
+    backgroundColor: "#fff",
+    borderBottom: "solid 1px #ddd",
+    borderLeft: "solid 1px #ddd",
+    borderRadius: 2,
+    borderRight: "solid 1px #ddd",
+    display: "flex",
+    justifyContent: "center",
+    padding: "10px 20px",
+  },
+  link: {
+    alignItems: "center",
+    display: "flex",
+    color: "#2DC7FF",
+    fontFamily:
+      '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
+    fontSize: 14,
+    fontWeight: 600,
+    lineHeight: 1.2,
+    margin: "0",
+  },
+  linkIcon: {
+    fill: "currentColor",
+    height: 20,
+    margin: "0 10px 0 0",
+    width: 20,
+  },
+};
